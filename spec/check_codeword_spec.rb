d9d322185cdb7a1df18e22cf4d5f9b59719f3409
William Lines <williamlines42@gmail.com>
2022-10-11 11:21:57 +0100

    improved spec for check_codeword

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,15 +1,23 @@
 require 'check_codeword'
 
 RSpec.describe "check_codeword method" do
-  it "returns 'Correct! Come in.' when passed 'horse'" do
+  it "returns 'Correct! Come in.' when passed 'horse' (correct answer)" do
     expect(check_codeword("horse")).to eq "Correct! Come in."
   end
   
-  it "returns 'Close, but nope.' when passed 'haste'" do
+  it "returns 'Close, but nope.' when passed 'haste'(nearly correct answer)" do
     expect(check_codeword("haste")).to eq "Close, but nope."
   end
   
-  it "returns 'WRONG!' when passed 'chain'" do
+  it "returns 'WRONG!' when passed 'chain'(incorrect answer)" do
+    expect(check_codeword("chain")).to eq "WRONG!"
+  end
+
+  it "returns 'WRONG!' when passed 'hairy'(correct first letter only)" do
+    expect(check_codeword("chain")).to eq "WRONG!"
+  end
+
+  it "returns 'WRONG!' when passed 'paste'(correct final letter only)" do
     expect(check_codeword("chain")).to eq "WRONG!"
   end
 end
